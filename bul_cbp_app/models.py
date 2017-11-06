308882e051f57daf38c2234c965cc5205bf1aa66
birkin <birkin.diana@gmail.com>
2017-11-06 11:00:23 -0500

    starts db.

--- bul_cbp_app/models.py
+++ bul_cbp_app/models.py
@@ -7,3 +7,14 @@ from django.db import models
 from django.http import HttpResponseRedirect
 
 log = logging.getLogger(__name__)
+
+
+class Tracker(models.Model):
+    created = models.DateTimeField( auto_now_add=True )
+    modified = models.DateTimeField( auto_now=True )
+    project_name = models.CharField( max_length=50 )
+
+    notes = models.TextField( null=True, blank=True )
+
+    def __unicode__(self):
+        return self.project_name
