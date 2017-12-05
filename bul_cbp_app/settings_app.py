8a104f31186fccc511aee01679ba9b29fec0080d
birkin <birkin.diana@gmail.com>
2017-12-05 17:05:23 -0500

    adds test shib-meta-dct.

--- bul_cbp_app/settings_app.py
+++ bul_cbp_app/settings_app.py
@@ -6,3 +6,16 @@ import json, os
 FOO = 'BAR'
 
 DEV_SHIB_INFO = json.loads( os.environ['DEV_SHIB_INFO_JSON'] )
+
+# TEST_USERNAME = os.environ['BUL_CBP__TEST_USERNAME']
+# TEST_NETID = os.environ['BUL_CBP__TEST_NETID']
+# TEST_EMAIL = os.environ['BUL_CBP__TEST_EMAIL']
+
+TEST_META_DCT = json.loads( os.environ['BUL_CBP__TEST_META_DCT_JSON'] )
+
+SUPER_USERS = json.loads( os.environ['BUL_CBP__SUPER_USERS_JSON'] )
+
+STAFF_USERS = json.loads( os.environ['BUL_CBP__STAFF_USERS_JSON'] )  # can use admin
+
+STAFF_GROUP = os.environ['BUL_CBP__STAFF_GROUP']
+
