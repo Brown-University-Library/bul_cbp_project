5dd504adc991b871aeac6ab2f27dea2445627efe
birkin <birkin.diana@gmail.com>
2018-02-24 07:09:10 -0500

    adds minimal test for score.

--- bul_cbp_app/tests.py
+++ bul_cbp_app/tests.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from .models import Tracker
+from bul_cbp_app.lib import image_helper
 from django.test import TestCase
-# from django.test import SimpleTestCase as TestCase    ## TestCase requires db, so if you're not using a db, and want tests, try this
 
 
 log = logging.getLogger(__name__)
 TestCase.maxDiff = None
+
+
+class Scorer( TestCase ):
+    """ Checks score calculation. """
+
+    def test_mostly_empty(self):
+        """ Checks single `yes`. """
+        trckr = Tracker( code_versioned='yes' )
+        self.assertEqual( 3, image_helper.calc_score(trckr) )
 
 
 class RootUrlTest( TestCase ):
@@ -25,5 +35,3 @@ class RootUrlTest( TestCase ):
         self.assertEqual( 302, response.status_code )  # permanent redirect
         redirect_url = response._headers['location'][1]
         self.assertEqual(  '/info/', redirect_url )
-
-    # end class RootUrlTest()
